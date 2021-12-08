8e663098fc14b2d08e974b28dba98430a0493c41
soren1338@penguin <soren1338@penguin.com>
2021-12-08 16:45:44 -0500

    Added some dom manipulation

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log('Hello world!')
+let health = 100
+let hunger = 0
+let happiness = 100 
+
+const buttons = document.querySelectorAll('button')
+buttons.forEach(button => {
+	button.onclick(
+		() => console.log(this.name)
+	)
+})
+
+const fills = document.querySelectorAll('.fill')
+console.log(fills)
+
+fills.forEach(fill => {
+	fill.style.width = `60%`
+})
+
+
+/*
+Functions needed:
+	Incrementer
+		modifies corresponding value, then accordingly modifies bar
+		takes "button ID" as a parameter
+
+	Time ticker
+		every X seconds applies aging modifiers
+
+*/
