74cea42c92af1c2a31d327444a8a29d6d671d458
soren1338@penguin <soren1338@penguin.com>
2021-12-11 14:03:16 -0500

    Refactored stat updates; working time updates

--- index.js
+++ index.js
@@ -1,53 +1,98 @@
-let health = 0
-let hunger = 100
-let happiness = 0 
+let health = 100
+let hunger = 0
+let happiness = 100 
+let isAlive = true
+let petAge = 0
+
+const healthFill = document.querySelector('#health-fill')
+const hungerFill = document.querySelector('#hunger-fill')
+const happinessFill = document.querySelector('#happiness-fill')
+
+const petNameArea = document.querySelector('#pet-name')
+const petAgeArea = document.querySelector('#pet-age')
+console.log(petAgeArea)
+
 
 const reset = () => {
-	health = 0
-	hunger = 100
-	happiness = 0 
-	document.querySelector('#health-fill').style.width = `${health}%`
-	document.querySelector('#hunger-fill').style.width = `${hunger}%`
-	document.querySelector('#happiness-fill').style.width = `${happiness}%`
+	health = 100
+	hunger = 0
+	happiness = 100 
+	isAlive = true
+
+	const petName = prompt('Please type the name of your tamagotchi')
+	document.querySelector('#pet-name').innerText = petName
+
+	healthFill.style.width = `${health}%`
+	hungerFill.style.width = `${hunger}%`
+	happinessFill.style.width = `${happiness}%`
 }
 reset()
 
+const confineValue = value => {
+	let confinedValue = value
+	if (value > 100) {confinedValue = 100}
+	if (value < 0) {confinedValue = 0}
+	return confinedValue
+}
+
+const statUpdate = (stat, increment) => {
+	switch (stat) {
+		case 'health':
+			health += increment
+			health = confineValue(health)
+			healthFill.style.width = `${health}%`
+			console.log(`Health: ${health}`)
+			console.log(`Health fill: ${healthFill.style.width}`)
+			break
+		case 'hunger':
+			hunger += increment
+			hunger = confineValue(hunger)
+			hungerFill.style.width = `${hunger}%`
+			console.log(`Hunger: ${hunger}`)
+			console.log(`Hunger fill: ${hungerFill.style.width}`)
+			break
+		case 'happiness':
+			happiness += increment
+			happiness = confineValue(happiness)
+			happinessFill.style.width = `${happiness}%`
+			console.log(`Happiness: ${happiness}`)
+			console.log(`Happiness fill: ${happinessFill.style.width}`)
+			break
+	}
+}
+
 const statTimer = interval => {
 	timeTicker = setInterval(() => {
-		health -= 10
-		document.querySelector('#health-fill').style.width = `${health}%`
-		hunger += 10
-		document.querySelector('#hunger-fill').style.width = `${hunger}%`
-		happiness -= 10 
-		document.querySelector('#happiness-fill').style.width = `${happiness}%`
+		petAge++
+		petAgeArea.innerText = `${petAge} days`
+		console.log(petAge)
+		statUpdate('health', -10)
+		statUpdate('hunger', 10)
+		statUpdate('happiness', -10)
+		if (health <= 0) {
+			alert('Game over, your pet is dead')
+			clearInterval(timeTicker)
+			isAlive = false
+		}
 	}, interval * 1000
 	)
 }
 
-// statUpdater(3)
+statTimer(1)
 
 const healthButton = document.querySelector('#health-button')
 healthButton.addEventListener('click', () => {
-	if (health < 100) {
-		health += 5
-		document.querySelector('#health-fill').style.width = `${health}%`
-	}
+	if (isAlive) {statUpdate('health', 5)}
 })
 
 const hungerButton = document.querySelector('#hunger-button')
 hungerButton.addEventListener('click', () => {
-	if (hunger > 0) {
-		hunger -= 5
-		document.querySelector('#hunger-fill').style.width = `${hunger}%`
-	}
+	if (isAlive) {statUpdate('hunger', -5)}
 })
 
 const happinessButton = document.querySelector('#happiness-button')
 happinessButton.addEventListener('click', () => {
-	if (happiness < 100) {
-		happiness += 5
-		document.querySelector('#happiness-fill').style.width = `${happiness}%`
-	}
+	if (isAlive) {statUpdate('happiness', 5)}
 })
 
 
