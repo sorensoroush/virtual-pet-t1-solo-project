57a6fb2550b619592989c4dad9d537beee08111e
soren1338@penguin <soren1338@penguin.com>
2021-12-11 11:11:50 -0500

    Scrapped refactor

--- index.js
+++ index.js
@@ -3,12 +3,28 @@ let hunger = 100
 let happiness = 0 
 
 const reset = () => {
+	health = 0
+	hunger = 100
+	happiness = 0 
 	document.querySelector('#health-fill').style.width = `${health}%`
 	document.querySelector('#hunger-fill').style.width = `${hunger}%`
 	document.querySelector('#happiness-fill').style.width = `${happiness}%`
 }
-
 reset()
+
+const statTimer = interval => {
+	timeTicker = setInterval(() => {
+		health -= 10
+		document.querySelector('#health-fill').style.width = `${health}%`
+		hunger += 10
+		document.querySelector('#hunger-fill').style.width = `${hunger}%`
+		happiness -= 10 
+		document.querySelector('#happiness-fill').style.width = `${happiness}%`
+	}, interval * 1000
+	)
+}
+
+// statUpdater(3)
 
 const healthButton = document.querySelector('#health-button')
 healthButton.addEventListener('click', () => {
@@ -25,6 +41,7 @@ hungerButton.addEventListener('click', () => {
 		document.querySelector('#hunger-fill').style.width = `${hunger}%`
 	}
 })
+
 const happinessButton = document.querySelector('#happiness-button')
 happinessButton.addEventListener('click', () => {
 	if (happiness < 100) {
